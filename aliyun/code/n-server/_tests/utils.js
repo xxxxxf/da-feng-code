a3b42657247e172c8bfc3478c384c94a15a11ef1
xxxxxf <544863848@qq.com>
2020-07-10 11:51:26 +0800

    ali

---
+++ aliyun/code/n-server/_tests/utils.js
@@ -0,0 +1,7 @@
+module.exports = {
+  nickname: '无双',
+  email: `admin${Math.floor(Math.random() * 10000)}@jiankou.top`,
+  password: `admin${Math.floor(Math.random() * 10000)}`,
+  name: `name${Math.floor(Math.random() * 10000)}`,
+  key: `key${Math.floor(Math.random() * 10000)}`,
+}
---
+++ aliyun/code/n-web/admin/src/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 style="text-align: center;margin-top: 100px">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+    data () {
+      return {
+        msg: '命格无双 ， 一统江山 !'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    font-weight: normal;
+  }
+</style>
